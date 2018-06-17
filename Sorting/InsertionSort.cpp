f74153f3e163d2f6334cf2a41a62c1c36febfd2d
Avinash Kumar Konda <avinashkumarkonda@hotmail.com>
2018-06-17 16:03:56 +0530

    Initial test commit for the C++ project

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,18 @@
+/*
+ * InsertionSort.cpp
+ *
+ *  Created on: 17-Jun-2018
+ *      Author: avinash
+ */
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Insertion sort";
+	return 0;
+}
